68d8a085fe0bd7a9cad16784c7f397230093c67b
Tom <tom.huynh@epita.fr>
2025-10-25 16:39:56 +0200

    removed NN related things, not need right now

--- src/core/image/image.h
+++ src/core/image/image.h
@@ -5,7 +5,7 @@
 #include <gtk/gtk.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <string.h>
-#include "../../nn/tensor.h"
+// #include "../../nn/tensor.h"
 struct Contour;
 
 typedef struct Image {
@@ -22,5 +22,5 @@ void save_image(const char* path, Image* image);
 void cpy_image(const Image* image, Image* image_cpy);
 void draw_rectangle(Image* image, int x, int y, int width, int height, bool fill, int thickness, uint32_t color);
 void extract_rectangle(const Image* image, int x, int y, int width, int height, Image* extracted_image);
-Tensor* to_tensor(Image* image);
+// Tensor* to_tensor(Image* image);
 void free_image(Image* image);
--- src/nn/activations.h
+++
@@ -1,6 +0,0 @@
-#include "tensor.h"
-
-#pragma once
-
-Tensor* sigmoid(Tensor* a);
-Tensor* sigmoid_grad(Tensor* a);
